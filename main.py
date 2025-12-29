825d581fa55aee6439ec94426fb5d4f506af0cec
Ömer Taha Öztop <taha.oztop@mono.tr>
2025-12-29 20:48:30 +0300

    fix SSL cert error in binary

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import os
+import certifi
+
+os.environ.setdefault("SSL_CERT_FILE", certifi.where())
+
 from textual.app import App
 from tui import ProjectBoardTree
+
 
 class PlankaApp(App):
     CSS_PATH = "tui.css"
@@ -7,6 +13,7 @@ class PlankaApp(App):
 
     def on_mount(self) -> None:
         self.push_screen(ProjectBoardTree())
+
 
 if __name__ == "__main__":
     app = PlankaApp()
